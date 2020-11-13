eb1dd2d9950ecd8da8a2025434ef410ada68ba41
Remus Courtenay <remuscourtenay@gmail.com>
2020-11-13 16:29:35 +1300

    Structured some program interface classes for interacting with ActiveWorks and SportsCore

---
+++ src/main/java/programInterfaces/SportsCoreInterface.java
@@ -0,0 +1,12 @@
+package programInterfaces;
+
+/**
+ * Implementation of ProgramInterface that specifically handles data coming from and going to the SportsCore program.
+ * SportsCore assigns a time and ranking to event participants as they cross the finish line. It does so by linking
+ * the participants to their result via a chip number.
+ *
+ * @author Remus Courtenay - rcou199
+ * @since 13/11/2020
+ */
+public class SportsCoreInterface implements ProgramInterface {
+}
