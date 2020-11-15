fc44be2ba0cfaddb96c3aac9f3c153534b07b9d2
Remus Courtenay <remuscourtenay@gmail.com>
2020-11-16 10:53:24 +1300

    Set up default format types

--- src/main/java/dosStuff/fileCreators/FormatTypesFileCreator.java
+++ src/main/java/dosStuff/fileCreators/FormatTypesFileCreator.java
@@ -10,8 +10,22 @@ public class FormatTypesFileCreator extends FileCreator {
 
     private static final String DATA_TYPES_FILE_COMMENT = "todo"; // TODO...
 
+    private static final String GENERAL_FORMAT = "General";
+
     private static final String[][] DEFAULT_FORMAT_TYPES = {
-            {"NAME", "=NOT(EXACT([cell],PROPER([cell]))"}
+            {NONE,          GENERAL_FORMAT},
+            {TEXT,          GENERAL_FORMAT},
+            {ADDRESS,       GENERAL_FORMAT},
+            {POST_CODE,     GENERAL_FORMAT},
+            {EMAIL,         GENERAL_FORMAT},
+            {PHONE_NUMBER,  GENERAL_FORMAT},
+            {GENDER,        GENERAL_FORMAT},
+            {ID_NUMBER,     GENERAL_FORMAT},
+            {TAG_NUMBER,    GENERAL_FORMAT},
+            {TIME,          "dd-mm-yyyy_h:ss"}, // Doesn't add zeros to hours<10
+            {DISTANCE,      GENERAL_FORMAT},
+            {DATE,          "dd-mm-yyyy"},
+            {EVENT,         GENERAL_FORMAT}
     };
 
     @Override
