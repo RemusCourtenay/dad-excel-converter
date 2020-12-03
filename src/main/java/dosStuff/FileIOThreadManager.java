4cdcbec04450f85f9f08b00ea531322f3ef1020f
RemusCourtenay <remuscourtenay@gmail.com>
2020-12-03 18:55:49 +1300

    Removed unnecessary Executors from FileIOThreadManager class

--- src/main/java/dosStuff/FileIOThreadManager.java
+++ src/main/java/dosStuff/FileIOThreadManager.java
@@ -18,26 +18,19 @@ public class FileIOThreadManager {
     private static final int NUM_COMMENT_LINES = 1;
 
     private final String fileAddress;
-    private final ExecutorService commandExecutor;
     private final DataFileReader dataFileReader;
     private final FileCreator fileCreator;
 
 
     public FileIOThreadManager(String fileAddress, DataFileReader dataFileReader, FileCreator fileCreator) {
         this.fileAddress = fileAddress;
-        this.commandExecutor = Executors.newSingleThreadExecutor();
         this.dataFileReader = dataFileReader;
         this.fileCreator = fileCreator;
     }
 
     public synchronized void setupFile() {
         if (!BatchFileHandler.fileExists(fileAddress)) {
-            commandExecutor.submit(new Runnable() {
-                @Override
-                public void run() {
-                    fileCreator.createDefaultFile(fileAddress);
-                }
-            });
+            fileCreator.createDefaultFile(fileAddress);
         }
     }
 
@@ -45,26 +38,8 @@ public class FileIOThreadManager {
     public synchronized List<String[]> readFile() {
 
         if (BatchFileHandler.fileExists(fileAddress)) {
-            List<String[]> dataArrays = new ArrayList<>();
 
-            commandExecutor.submit(new Runnable() {
-                @Override
-                public void run() {
-                    dataArrays.addAll(dataFileReader.readFromFile(fileAddress));
-                }
-            });
-            commandExecutor.shutdown();
-
-            try {
-                if (!commandExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
-                    commandExecutor.shutdownNow();
-                    throw new RuntimeException("Executor timed out trying to read file: " + fileAddress);
-                }
-            } catch (InterruptedException exception) {
-                exception.printStackTrace();
-                throw new RuntimeException("Executor was interrupted while trying to read file: " + fileAddress);
-            }
-            return dataArrays;
+            return new ArrayList<>(dataFileReader.readFromFile(fileAddress));
         } else {
             throw new RuntimeException("Attempting to read file: " + fileAddress + " before it has been created");
         }
