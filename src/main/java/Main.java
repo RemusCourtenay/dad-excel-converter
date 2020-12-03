17f323076b5fe18e529c922bf21cd7922210210d
RemusCourtenay <remuscourtenay@gmail.com>
2020-12-03 19:06:03 +1300

    Scaffolded basic layout of how column objects will be built from the save data files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@
 import dosStuff.FileHandler;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 /**
  * @author Remus Courtenay - rcou199
@@ -12,7 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        new FileHandler();
+        FileHandler fileHandler = new FileHandler();
+
+        List<Column> masterSheetColumns = ColumnBuilder.fromDataFile(fileHandler.getFile(DataFileType.MasterSheetLayout));
     }
 
 
