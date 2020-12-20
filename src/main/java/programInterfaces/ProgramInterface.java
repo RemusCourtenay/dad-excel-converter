10ea6a25bf527d9dfec5209c376e4ce2cbfa741c
RemusCourtenay <remuscourtenay@gmail.com>
2020-12-20 13:06:32 +1300

    Added shared code and constructor to ProgramInterface

--- src/main/java/programInterfaces/ProgramInterface.java
+++ src/main/java/programInterfaces/ProgramInterface.java
@@ -1,10 +1,21 @@
 package programInterfaces;
 
+import org.apache.poi.ss.usermodel.Workbook;
+
 /**
  * Interface that represents all classes that interface this converter program with some other program.
  *
  * @author Remus Courtenay - rcou199
  * @since 13/11/2020
  */
-public interface ProgramInterface {
+public abstract class ProgramInterface { // TODO... Comment
+
+    private final Workbook generatedWorkbook;
+
+    public ProgramInterface(Workbook saveDataWorkbook) {
+        generatedWorkbook = loadSaveDataWorkbook(saveDataWorkbook);
+    }
+
+    protected abstract Workbook loadSaveDataWorkbook(Workbook saveDataWorkbook);
+
 }
